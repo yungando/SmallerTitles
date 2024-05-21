87618b815b1ad66952da6484994291e24c01967e
Levoment <30224138+Levoment@users.noreply.github.com>
2024-05-20 22:37:31 -0500

    1.20.4 version

--- src/client/java/com/github/levoment/mixin/client/SmallTitlesClientMixin.java
+++ src/client/java/com/github/levoment/mixin/client/SmallTitlesClientMixin.java
@@ -12,8 +12,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(InGameHud.class)
 public class SmallTitlesClientMixin {
-	@Redirect(method = "renderTitleAndSubtitle",
-	at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)I", ordinal = 0))
+	@Redirect(method = "render",
+	at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)I", ordinal = 1))
 	private int SmallerTitlesMod$changeTitleLocation(DrawContext instance, TextRenderer textRenderer, Text text, int x, int y, int color) {
 		if (SmallerTitlesConfig.showTitles) {
 			return instance.drawTextWithShadow(textRenderer, text, x - SmallerTitlesConfig.titleXOffset, y - SmallerTitlesConfig.titleYOffset, color);
@@ -23,14 +23,14 @@ public class SmallTitlesClientMixin {
 		}
 	}
 
-	@Redirect(method = "renderTitleAndSubtitle",
+	@Redirect(method = "render",
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;scale(FFF)V", ordinal = 0))
 	private void SmallerTitlesMod$changeTitleSize(MatrixStack matrixStack, float x, float y, float z) {
 		if (SmallerTitlesConfig.showTitles) matrixStack.scale(SmallerTitlesConfig.titleScaleInX, SmallerTitlesConfig.titleScaleInY, SmallerTitlesConfig.titleScaleInZ);
 	}
 
-	@Redirect(method = "renderTitleAndSubtitle",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)I", ordinal = 1))
+	@Redirect(method = "render",
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)I", ordinal = 2))
 	private int SmallerTitleMod$changeSubtitleLocation(DrawContext instance, TextRenderer textRenderer, Text text, int x, int y, int color) {
 		if (SmallerTitlesConfig.showSubtitles) {
 			return instance.drawTextWithShadow(textRenderer, text, x - SmallerTitlesConfig.subtitleXOffset, y - SmallerTitlesConfig.subtitleYOffset, color);
@@ -40,7 +40,7 @@ public class SmallTitlesClientMixin {
 		}
 	}
 
-	@Redirect(method = "renderTitleAndSubtitle",
+	@Redirect(method = "render",
 			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;scale(FFF)V", ordinal = 1))
 	private void changeTextSize(MatrixStack matrixStack, float x, float y, float z) {
 		if (SmallerTitlesConfig.showSubtitles) matrixStack.scale(SmallerTitlesConfig.subtitleScaleInX, SmallerTitlesConfig.subtitleScaleInY, SmallerTitlesConfig.subtitleScaleInZ);
